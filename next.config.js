e1b651ea18ad3eb1fb5b2e067033689b04a40cad
Sanmathi-Sedhupathi <sanmathisedhupathi2004@gmail.com>
2025-06-21 08:16:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // <--- Add this line
+  },
   images: {
     domains: [
       "res.cloudinary.com",
